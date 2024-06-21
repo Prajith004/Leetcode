51ed95775f419cfec09b0d4f7063ad2de2df12d1
Prajith J Shetty <79052637+Prajith004@users.noreply.github.com>
2024-06-21 15:29:25 +0530

    Time: 27 ms (92.36%) | Memory: 16.5 MB (83.33%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -2,9 +2,9 @@ class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         result = ""
         while columnNumber > 0:
-            remainder = (columnNumber - 1) % 26  # Convert 1-based to 0-based index
-            result = chr(65 + remainder) + result  # Convert remainder to corresponding letter
-            columnNumber = (columnNumber - 1) // 26  # Update column number for next iteration
+            remainder = (columnNumber - 1) % 26 
+            result = chr(65 + remainder) + result  
+            columnNumber = (columnNumber - 1) // 26  
         return result
 
             
