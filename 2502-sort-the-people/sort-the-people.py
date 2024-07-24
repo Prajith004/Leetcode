7874ce09e17e1985caa3849b80352c75a5343a1c
Prajith J Shetty <79052637+Prajith004@users.noreply.github.com>
2024-07-24 20:37:19 +0530

    Time: 94 ms (93.46%) | Memory: 17 MB (62.19%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,16 +1,11 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        
-        r_d=list(zip(names,heights))
-        r_l=[]
-        sorted_keys = sorted(r_d, key=lambda x: x[1],reverse=True)
+
+        r_d = list(zip(names, heights))
+        r_l = []
+        sorted_keys = sorted(r_d, key=lambda x: x[1], reverse=True)
         for i in range(len(sorted_keys)):
-            
+
             r_l.append(sorted_keys[i][0])
 
         return r_l
-        
-
-
-
-        
